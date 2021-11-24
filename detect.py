f7d12741cedde5c6f0d0ac982dc6b2a5ed4f633f
kretmatt <mrtthias@gmail.com>
2021-11-24 13:13:24 +0100

    Comment gender detection for main branch

--- detect.py
+++ detect.py
@@ -73,10 +73,10 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
                     crop_img = im0[(int(xyxy[1])-10):(int(xyxy[3])-10),(int(xyxy[0])+10):(int(xyxy[2])+10)]
-                    gender = genderer.detect_gender(crop_img)
+                    #gender = genderer.detect_gender(crop_img)
                     fps = 1/(time.time()-start_time)
                     #add text to label to display result
-                    label = f' {conf:.2f}  {round(fps,2)} {gender}'
+                    label = f' {conf:.2f}  {round(fps,2)}'
                     #label += {gender}
                     annotator.box_label(xyxy, label, color=colors(0, True))
 
