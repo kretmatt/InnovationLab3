b174376bc21271d2aed6fafbafb6a32d9386cfd4
John De La Torre <if18b103@technikum-wien.at>
2021-12-15 16:15:43 +0100

    changes to the ui added

---
+++ detect.py
@@ -0,0 +1,153 @@
+#TODO: GPU verwendung
+#TODO: Andere Gender Det. ausprobieren
+#TODO: Documentation
+
+
+import argparse
+import os
+import sys
+import time
+import cv2
+import torch
+import numpy as np
+import torch.backends.cudnn as cudnn
+from pathlib import Path
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+from models.common import DetectMultiBackend
+from utils.datasets import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
+from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr,
+                           increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
+from utils.plots import Annotator, colors, save_one_box
+from utils.torch_utils import select_device, time_sync
+from utils.gendern import gender_detector
+from utils.age import age_detector
+
+
+@torch.no_grad()
+def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
+        source=ROOT / 'data/images',  # file/dir/URL/glob, 0 for webcam
+        conf_thres=0.25,  # confidence threshold
+        iou_thres=0.45,  # NMS IOU threshold
+        gen_det=False,
+        age_det=False,
+        ):
+
+    source = str(source)
+    webcam = source.isnumeric()
+    genderer = gender_detector(Path.cwd())
+    age_modeler = age_detector(Path.cwd())
+
+    # Load model
+    device = select_device("")
+    model = DetectMultiBackend(weights, device=device, dnn=False)
+    stride, names, pt, jit, onnx = model.stride, model.names, model.pt, model.jit, model.onnx
+    imgsz = check_img_size(640, s=stride)  # check image size
+
+    # Dataloader
+    dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt and not jit)
+
+    # Run inference
+    for path, im, im0s in dataset:
+        im = torch.from_numpy(im).to(device)
+        im = im.float()  # uint8 to fp16/32
+        im /= 255  # 0 - 255 to 0.0 - 1.0
+        if len(im.shape) == 3:
+            im = im[None]  # expand for batch dim
+        start_time = time.time()
+        # Inference
+        pred = model(im, augment=False, visualize=False)
+
+        # NMS
+        pred = non_max_suppression(pred, conf_thres, iou_thres, max_det=1000)
+
+        # Process predictions
+        for i, det in enumerate(pred):  # per image
+            p, im0, frame = path[i], im0s[i].copy(), dataset.count
+
+            # test code
+            origin_h, origin_w = im0.shape[:2]
+
+            p = Path(p)  # to Path
+            annotator = Annotator(im0, line_width=3, example=str(names))
+            if len(det): #check if detection is not empty
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round() * np.array([1,1,.9,.9]) # resize box to face # Todo: find a better way to do this, causes more undetectable of objects
+                #bounding_box = det[:, :4] * np.array([origin_w, origin_h, origin_w, origin_h])
+                #x_start, y_start, x_end, y_end = bounding_box.int()
+
+                #det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    #add text to label to display result
+                    #label = f'Conf:{conf:.2f} '
+                    label = ''
+                    box_col = 8 # default color
+                    if(age_det is True or gen_det is True):
+                        crop_img = im0[(int(xyxy[1])):(int(xyxy[3])),(int(xyxy[0])):(int(xyxy[2]))] # Almost no impact on FPS counter
+                        #age detection code
+                        if(age_det is True):
+                            age = age_modeler.detect_age(crop_img)
+                            #label += f'Age: {age} '
+                            label += f'{age}'
+                        # gender detection codeq
+                        if(gen_det is True):
+                            gender = genderer.detect_gender(crop_img) # Significant FPS drop (Matthias: around 3 FPS)
+                            #label += f'Gender: {gender}'
+                            label += f', {gender}'
+                            # color changer 
+                            if(gender  == "Male"):
+                                #print(gender)
+                                box_col = 0
+                            elif(gender == "Female"):
+                                #print(gender)
+                                box_col = 4
+                            else:
+                                #print("nothing detected")
+                                box_col = 8
+                    # combine fps and conf 
+                    fps = 1/(time.time()-start_time)
+                    fps_conf_ctnr = f'Conf:{conf:.2f} '
+                    fps_conf_ctnr += f'FPS: {round(fps,2)}'
+                    cv2.putText(im0, fps_conf_ctnr, (15, 30), cv2.FONT_HERSHEY_SIMPLEX, .75, (255, 255, 255), 2) # put fps counter on the top left corner
+                    #print(xyxy)
+                    annotator.box_label(xyxy, label, color = colors(box_col, True))
+
+            # Stream results
+            im0 = annotator.result()
+            #if view_img:
+            #fps_ctnr = f'FPS: {round(fps,2)}'
+            
+            cv2.imshow(str(p), im0)
+            cv2.waitKey(1)  # 1 millisecond
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path(s)')
+    parser.add_argument('--source', type=str, default='0', help='file/dir/URL/glob, 0 for webcam')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+    parser.add_argument('--gen-det', type=bool, default=False, help='gender detection, default false')
+    parser.add_argument('--age-det', type=bool, default=False, help='age detection, default false')
+    opt = parser.parse_args()
+    #opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
+    print_args(FILE.stem, opt)
+    return opt
+
+
+def main(opt):
+    check_requirements(exclude=('tensorboard', 'thop'))
+    print(vars(opt))
+    run(**vars(opt))
+
+
+if __name__ == "__main__":
+    opt = parse_opt()
+    main(opt)
---
+++ utils/age.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+import keras
+import tensorflow as tf
+
+class age_detector:
+
+    def __init__(self, root):
+        #self.age_model = cv2.dnn.readNetFromCaffe(str(root) + '/deploy_age.prototxt', str(root) + '/age_net.caffemodel')
+        self.age_model = tf.keras.models.load_model('age_model.h5')
+        self.label_to_age_map = {
+            0: '0-2',
+            1: '4-6',
+            2: '8-13',
+            3: '15-20',
+            4: '25-32',
+            5: '38-43',
+            6: '48-53',
+            7: '60+'
+        }
+
+    def detect_age(self, img_face):
+        try:
+            """
+            MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+            ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+            img_blob = cv2.dnn.blobFromImage(img_face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
+            self.age_model.setInput(img_blob)
+            age_class = self.age_model.forward()
+            age = ageList[age_class[0].argmax()]
+            """
+            gray = cv2.cvtColor(img_face, cv2.COLOR_BGR2GRAY)
+            resized = cv2.resize(gray, (32, 32))
+            normalized = resized / 255.0
+            reshaped = np.reshape(normalized, (1, 32, 32, 1))
+            result = self.age_model.predict(reshaped)
+            label = np.argmax(result, axis=1)[0]
+            age = self.label_to_age_map[label]
+            return age
+        except Exception as e:
+            print(str(e))
+            return 'UNDETECTED'
+
+
+
+
