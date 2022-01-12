605ec6ca83171a30251119013900bb38cdb0ac45
kretmatt <mrtthias@gmail.com>
2022-01-12 18:36:27 +0100

    Combine gui with detect

--- detect.py
+++ detect.py
@@ -10,6 +10,7 @@ import torch
 import numpy as np
 import torch.backends.cudnn as cudnn
 from pathlib import Path
+import threading
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
@@ -25,81 +26,102 @@ from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, time_sync
 from utils.prediction import prediction
 
-
-@torch.no_grad()
-def run(weights=ROOT / 'models/face_detection_yolov5s.pt',  # model.pt path(s)
-        source=ROOT / 'data/images',  # file/dir/URL/glob, 0 for webcam
+class inn3_detector:
+    def __init__(self, weights=ROOT / 'models/face_detection_yolov5s.pt',  # model.pt path(s)
+        source='0',  # file/dir/URL/glob, 0 for webcam
         conf_thres=0.25,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
         gen_det=False,
         age_det=False,
+        emo_det=False
         ):
+        self.source = str(source)
+        self.predictor = prediction(gen_det, age_det, emo_det)
+        self.device = select_device("")
+        self.model = DetectMultiBackend(weights, device=self.device, dnn=False)
+        self.conf_thres=conf_thres
+        self.iou_thres=iou_thres
+        self.current_pic=np.array([])
 
-    source = str(source)
-    webcam = source.isnumeric()
-    predictor = prediction(gen_det,age_det)
+        # Load model
+        self.stride, self.names, self.pt, self.jit, self.onnx = self.model.stride, self.model.names, self.model.pt, self.model.jit, self.model.onnx
+        self.imgsz = check_img_size(640, s=self.stride)  # check image size
 
-    # Load model
-    device = select_device("")
-    model = DetectMultiBackend(weights, device=device, dnn=False)
-    stride, names, pt, jit, onnx = model.stride, model.names, model.pt, model.jit, model.onnx
-    imgsz = check_img_size(640, s=stride)  # check image size
+        # Dataloader
+        self.dataset = LoadStreams(self.source, img_size=self.imgsz, stride=self.stride, auto=self.pt and not self.jit)
 
-    # Dataloader
-    dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt and not jit)
-    predictor.start_threads()
-    # Run inference
-    for path, im, im0s in dataset:
-        im = torch.from_numpy(im).to(device)
-        im = im.float()  # uint8 to fp16/32
-        im /= 255  # 0 - 255 to 0.0 - 1.0
-        if len(im.shape) == 3:
-            im = im[None]  # expand for batch dim
-        start_time = time.time()
-        # Inference
-        pred = model(im, augment=False, visualize=False)
+    def set_agedet(self, age_det):
+        self.predictor.age_det=age_det
 
-        # NMS
-        pred = non_max_suppression(pred, conf_thres, iou_thres, max_det=1000)
+    def set_gendet(self, gen_det):
+        self.predictor.gen_det=gen_det
 
-        # Process predictions
-        for i, det in enumerate(pred):  # per image
-            p, im0, frame = path[i], im0s[i].copy(), dataset.count
+    def set_emodet(self, emo_det):
+        self.predictor.emo_det=emo_det
 
-            p = Path(p)  # to Path
-            annotator = Annotator(im0, line_width=3, example=str(names))
-            if len(det): #check if detection is not empty
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
-                crop_ims = []
-                # Write results
-                for *xyxy, conf, cls in reversed(det):
-                    crop_img = im0[(int(xyxy[1])):(int(xyxy[3])),(int(xyxy[0])):(int(xyxy[2]))] # Almost no impact on FPS counter
-                    crop_ims.append([xyxy, crop_img])
-                predictor.pass_detections(crop_ims.copy())
-                pres = predictor.results.copy()
-                for res in pres:
-                    box_col = 8 # default color
-                    boxtext = res[2] + " " + res[3] + " " + res[4]
-                    if(res[2]  == "Male"):
-                        #print(gender)
-                        box_col = 0
-                    elif(res[2] == "Female"):
-                        #print(gender)
-                        box_col = 4
-                    else:
-                        #print("nothing detected")
-                        box_col = 8
-                    annotator.box_label(res[0], boxtext, color=colors(box_col, True))
-            # Stream results
-            # combine fps and conf
-            fps = 1 / (time.time() - start_time)
-            fps_conf_ctnr = f'FPS:{fps:.2f} '
-            cv2.putText(im0, fps_conf_ctnr, (15, 30), cv2.FONT_HERSHEY_SIMPLEX, .75, (255, 255, 255),2)  # put fps counter on the top left corner
-            im0 = annotator.result()
-            #if view_img:
-            cv2.imshow(str(p), im0)
-            cv2.waitKey(1)  # 1 millisecond
+    def start_pred(self):
+        self.predictor.start_threads()
+
+    def get_currentpic(self):
+        return self.current_pic
+
+    def start_threads(self):
+        x = threading.Thread(target=self.run, args=([]), daemon=True)
+        x.start()
+
+    @torch.no_grad()
+    def run(self):
+        # Run inference
+        for path, im, im0s in self.dataset:
+            im = torch.from_numpy(im).to(self.device)
+            im = im.float()  # uint8 to fp16/32
+            im /= 255  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
+            # Inference
+            pred = self.model(im, augment=False, visualize=False)
+
+            # NMS
+            pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, max_det=1000)
+
+            # Process predictions
+            for i, det in enumerate(pred):  # per image
+                p, im0, frame = path[i], im0s[i].copy(), self.dataset.count
+
+                p = Path(p)  # to Path
+                annotator = Annotator(im0, line_width=3, example=str(self.names))
+                if len(det): #check if detection is not empty
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+                    crop_ims = []
+                    # Write results
+                    for *xyxy, conf, cls in reversed(det):
+                        crop_img = im0[(int(xyxy[1])):(int(xyxy[3])),(int(xyxy[0])):(int(xyxy[2]))] # Almost no impact on FPS counter
+                        crop_ims.append([xyxy, crop_img])
+                    self.predictor.pass_detections(crop_ims.copy())
+                    pres = self.predictor.results.copy()
+                    for res in pres:
+                        box_col = 8 # default color
+                        boxtext = res[2] + " " + res[3] + " " + res[4]
+                        if(res[2]  == "Male"):
+                            #print(gender)
+                            box_col = 0
+                        elif(res[2] == "Female"):
+                            #print(gender)
+                            box_col = 4
+                        else:
+                            #print("nothing detected")
+                            box_col = 8
+                        annotator.box_label(res[0], boxtext, color=colors(box_col, True))
+                # Stream results
+                # combine fps and conf
+
+                #cv2.putText(im0, fps_conf_ctnr, (15, 30), cv2.FONT_HERSHEY_SIMPLEX, .75, (255, 255, 255),2)  # put fps counter on the top left corner
+                im0 = annotator.result()
+                #if view_img:
+                self.current_pic=im0
+
+
 
 
 def parse_opt():
@@ -110,6 +132,7 @@ def parse_opt():
     parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
     parser.add_argument('--gen_det', type=bool, default=False, help='gender detection, default false')
     parser.add_argument('--age_det', type=bool, default=False, help='age detection, default false')
+    parser.add_argument('--emo_det', type=bool, default=False, help='emotion detection, default false')
     opt = parser.parse_args()
     #opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
     print_args(FILE.stem, opt)
@@ -119,8 +142,10 @@ def parse_opt():
 def main(opt):
     check_requirements(exclude=('tensorboard', 'thop'))
     print(vars(opt))
-    run(**vars(opt))
-
+    idet = inn3_detector(**vars(opt))
+    idet.start_pred()
+    idet.start_threads()
+    time.sleep(5)
 
 if __name__ == "__main__":
     opt = parse_opt()
--- utils/prediction.py
+++ utils/prediction.py
@@ -5,7 +5,7 @@ from utils.age import age_detector
 from utils.emotion import emotion_detector
 
 class prediction:
-    def __init__(self, gen_det, age_det):
+    def __init__(self, gen_det, age_det, emo_det):
         self.age_detector = age_detector()
         self.gender_detector = gender_detector()
         self.emotion_detector = emotion_detector()
@@ -13,6 +13,7 @@ class prediction:
         self.results = []
         self.gen_det=gen_det
         self.age_det=age_det
+        self.emo_det=emo_det
 
     def start_threads(self):
         x = threading.Thread(target=self.predict, args=([]), daemon=True)
@@ -26,19 +27,21 @@ class prediction:
             try:
                 newdets = []
                 for img in self.images:
-                    age=""
-                    gender=""
+                    age = ""
+                    gender = ""
+                    emotion = ""
                     # Predict gender
                     if(self.gen_det):
                         gender = self.gender_detector.detect_gender(img[1])
                     # Predict age
                     if(self.age_det):
                         age = self.age_detector.detect_age(img[1])
+                    # Predict emotion
+                    if (self.emo_det):
+                        emotion = self.emotion_detector.detect_emotion(img[1])
                     # Append results to newdets
-
-                    emotion = self.emotion_detector.detect_emotion(img[1])
                     newdets.append([img[0], img[1], gender, age, emotion])
                 self.results = newdets
             except Exception as e:
                 print(str(e))
-            time.sleep(1/10)
+            #time.sleep(1/10)
